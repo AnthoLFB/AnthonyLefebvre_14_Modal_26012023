e2a7649ec019ce6697105899bf6df83168765dd4
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-04 15:31:33 +0100

    Generation of the lib via rollupJS.

---
+++ dist/index.js
@@ -0,0 +1,103 @@
+var closeBtn = "<svg xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"50\" height=\"50\" viewBox=\"0 0 50 50\">\r\n    <path d=\"M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 32.990234 15.986328 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.990234 15.986328 z\"></path>\r\n</svg>";
+
+var warningLogo = "<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n<path d=\"M12 16.99V17M12 7V14M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z\" stroke=\"#906200\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/>\r\n</svg>";
+
+var informativeLogo = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 24 24\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\r\n    <title>information_line</title>\r\n    <g id=\"页面-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\r\n        <g id=\"System\" transform=\"translate(-672.000000, 0.000000)\" fill-rule=\"nonzero\">\r\n            <g id=\"information_line\" transform=\"translate(672.000000, 0.000000)\">\r\n                <path d=\"M24,0 L24,24 L0,24 L0,0 L24,0 Z M12.5934901,23.257841 L12.5819402,23.2595131 L12.5108777,23.2950439 L12.4918791,23.2987469 L12.4918791,23.2987469 L12.4767152,23.2950439 L12.4056548,23.2595131 C12.3958229,23.2563662 12.3870493,23.2590235 12.3821421,23.2649074 L12.3780323,23.275831 L12.360941,23.7031097 L12.3658947,23.7234994 L12.3769048,23.7357139 L12.4804777,23.8096931 L12.4953491,23.8136134 L12.4953491,23.8136134 L12.5071152,23.8096931 L12.6106902,23.7357139 L12.6232938,23.7196733 L12.6232938,23.7196733 L12.6266527,23.7031097 L12.609561,23.275831 C12.6075724,23.2657013 12.6010112,23.2592993 12.5934901,23.257841 L12.5934901,23.257841 Z M12.8583906,23.1452862 L12.8445485,23.1473072 L12.6598443,23.2396597 L12.6498822,23.2499052 L12.6498822,23.2499052 L12.6471943,23.2611114 L12.6650943,23.6906389 L12.6699349,23.7034178 L12.6699349,23.7034178 L12.678386,23.7104931 L12.8793402,23.8032389 C12.8914285,23.8068999 12.9022333,23.8029875 12.9078286,23.7952264 L12.9118235,23.7811639 L12.8776777,23.1665331 C12.8752882,23.1545897 12.8674102,23.1470016 12.8583906,23.1452862 L12.8583906,23.1452862 Z M12.1430473,23.1473072 C12.1332178,23.1423925 12.1221763,23.1452606 12.1156365,23.1525954 L12.1099173,23.1665331 L12.0757714,23.7811639 C12.0751323,23.7926639 12.0828099,23.8018602 12.0926481,23.8045676 L12.108256,23.8032389 L12.3092106,23.7104931 L12.3186497,23.7024347 L12.3186497,23.7024347 L12.3225043,23.6906389 L12.340401,23.2611114 L12.337245,23.2485176 L12.337245,23.2485176 L12.3277531,23.2396597 L12.1430473,23.1473072 Z\" id=\"MingCute\" fill-rule=\"nonzero\">\r\n\r\n</path>\r\n                <path d=\"M12,2 C17.5228,2 22,6.47715 22,12 C22,17.5228 17.5228,22 12,22 C6.47715,22 2,17.5228 2,12 C2,6.47715 6.47715,2 12,2 Z M12,4 C7.58172,4 4,7.58172 4,12 C4,16.4183 7.58172,20 12,20 C16.4183,20 20,16.4183 20,12 C20,7.58172 16.4183,4 12,4 Z M11.99,10 C12.5478,10 13,10.4522 13,11.01 L13,16.1338 C13.2989,16.3067 13.5,16.6299 13.5,17 C13.5,17.5523 13.0523,18 12.5,18 L12.01,18 C11.4521,18 11,17.5478 11,16.99 L11,12 C10.4477,12 10,11.5523 10,11 C10,10.4477 10.4477,10 11,10 L11.99,10 Z M12,7 C12.5523,7 13,7.44772 13,8 C13,8.55228 12.5523,9 12,9 C11.4477,9 11,8.55228 11,8 C11,7.44772 11.4477,7 12,7 Z\" id=\"形状\" fill=\"#0b508f\">\r\n\r\n</path>\r\n            </g>\r\n        </g>\r\n    </g>\r\n</svg>";
+
+var errorLogo = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\r\n\r\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\r\n\r\n<svg fill=\"#931a10\" width=\"800px\" height=\"800px\" viewBox=\"0 0 64 64\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\" xmlns:serif=\"http://www.serif.com/\" style=\"fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;\">\r\n\r\n<rect id=\"Icons\" x=\"-704\" y=\"-64\" width=\"1280\" height=\"800\" style=\"fill:none;\"/>\r\n\r\n<g id=\"Icons1\" serif:id=\"Icons\">\r\n\r\n<g id=\"Strike\">\r\n\r\n</g>\r\n\r\n<g id=\"H1\">\r\n\r\n</g>\r\n\r\n<g id=\"H2\">\r\n\r\n</g>\r\n\r\n<g id=\"H3\">\r\n\r\n</g>\r\n\r\n<g id=\"list-ul\">\r\n\r\n</g>\r\n\r\n<g id=\"hamburger-1\">\r\n\r\n</g>\r\n\r\n<g id=\"hamburger-2\">\r\n\r\n</g>\r\n\r\n<g id=\"list-ol\">\r\n\r\n</g>\r\n\r\n<g id=\"list-task\">\r\n\r\n</g>\r\n\r\n<g id=\"trash\">\r\n\r\n</g>\r\n\r\n<g id=\"vertical-menu\">\r\n\r\n</g>\r\n\r\n<g id=\"horizontal-menu\">\r\n\r\n</g>\r\n\r\n<g id=\"sidebar-2\">\r\n\r\n</g>\r\n\r\n<g id=\"Pen\">\r\n\r\n</g>\r\n\r\n<g id=\"Pen1\" serif:id=\"Pen\">\r\n\r\n</g>\r\n\r\n<g id=\"clock\">\r\n\r\n</g>\r\n\r\n<g id=\"external-link\">\r\n\r\n</g>\r\n\r\n<g id=\"hr\">\r\n\r\n</g>\r\n\r\n<g id=\"info\">\r\n\r\n</g>\r\n\r\n<g id=\"warning\">\r\n\r\n</g>\r\n\r\n<path id=\"error-circle\" d=\"M32.085,56.058c6.165,-0.059 12.268,-2.619 16.657,-6.966c5.213,-5.164 7.897,-12.803 6.961,-20.096c-1.605,-12.499 -11.855,-20.98 -23.772,-20.98c-9.053,0 -17.853,5.677 -21.713,13.909c-2.955,6.302 -2.96,13.911 0,20.225c3.832,8.174 12.488,13.821 21.559,13.908c0.103,0.001 0.205,0.001 0.308,0Zm-0.282,-4.003c-9.208,-0.089 -17.799,-7.227 -19.508,-16.378c-1.204,-6.452 1.07,-13.433 5.805,-18.015c5.53,-5.35 14.22,-7.143 21.445,-4.11c6.466,2.714 11.304,9.014 12.196,15.955c0.764,5.949 -1.366,12.184 -5.551,16.48c-3.672,3.767 -8.82,6.016 -14.131,6.068c-0.085,0 -0.171,0 -0.256,0Zm-12.382,-10.29l9.734,-9.734l-9.744,-9.744l2.804,-2.803l9.744,9.744l10.078,-10.078l2.808,2.807l-10.078,10.079l10.098,10.098l-2.803,2.804l-10.099,-10.099l-9.734,9.734l-2.808,-2.808Z\"/>\r\n\r\n<g id=\"plus-circle\">\r\n\r\n</g>\r\n\r\n<g id=\"minus-circle\">\r\n\r\n</g>\r\n\r\n<g id=\"vue\">\r\n\r\n</g>\r\n\r\n<g id=\"cog\">\r\n\r\n</g>\r\n\r\n<g id=\"logo\">\r\n\r\n</g>\r\n\r\n<g id=\"radio-check\">\r\n\r\n</g>\r\n\r\n<g id=\"eye-slash\">\r\n\r\n</g>\r\n\r\n<g id=\"eye\">\r\n\r\n</g>\r\n\r\n<g id=\"toggle-off\">\r\n\r\n</g>\r\n\r\n<g id=\"shredder\">\r\n\r\n</g>\r\n\r\n<g id=\"spinner--loading--dots-\" serif:id=\"spinner [loading, dots]\">\r\n\r\n</g>\r\n\r\n<g id=\"react\">\r\n\r\n</g>\r\n\r\n<g id=\"check-selected\">\r\n\r\n</g>\r\n\r\n<g id=\"turn-off\">\r\n\r\n</g>\r\n\r\n<g id=\"code-block\">\r\n\r\n</g>\r\n\r\n<g id=\"user\">\r\n\r\n</g>\r\n\r\n<g id=\"coffee-bean\">\r\n\r\n</g>\r\n\r\n<g id=\"coffee-beans\">\r\n\r\n<g id=\"coffee-bean1\" serif:id=\"coffee-bean\">\r\n\r\n</g>\r\n\r\n</g>\r\n\r\n<g id=\"coffee-bean-filled\">\r\n\r\n</g>\r\n\r\n<g id=\"coffee-beans-filled\">\r\n\r\n<g id=\"coffee-bean2\" serif:id=\"coffee-bean\">\r\n\r\n</g>\r\n\r\n</g>\r\n\r\n<g id=\"clipboard\">\r\n\r\n</g>\r\n\r\n<g id=\"clipboard-paste\">\r\n\r\n</g>\r\n\r\n<g id=\"clipboard-copy\">\r\n\r\n</g>\r\n\r\n<g id=\"Layer1\">\r\n\r\n</g>\r\n\r\n</g>\r\n\r\n</svg>";
+
+var successLogo = "<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n<path d=\"M17 9L9.99998 16L6.99994 13M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z\" stroke=\"#405d2a\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/>\r\n</svg>";
+
+//Images
+function Modal({
+  isOpen,
+  setIsOpen,
+  isScrollable,
+  type,
+  iconToDisplay,
+  title,
+  message
+}) {
+  //Display of a default icon if the modal belongs to a predefined type and no icon is specified.
+  const icon = iconToDisplay ?? getDefaultIcons(type);
+
+  //Allows to close the modal when clicking on the button and restores the possibility to scroll if it was blocked.
+  const closeModal = () => {
+    setIsOpen(false);
+    if (isScrollable == false) {
+      enableScroll();
+    }
+  };
+
+  //If the modal is open then the content of the modal is returned.
+  if (isOpen) {
+    //Disable the scroll if the user wants it.
+    if (isScrollable == false) {
+      disableScroll();
+    }
+    return /*#__PURE__*/React.createElement("section", {
+      className: "modal-container"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "modal modal--" + type
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "modal__header modal__header--" + type
+    }, /*#__PURE__*/React.createElement("h2", {
+      className: "modal__header__title"
+    }, type), /*#__PURE__*/React.createElement("button", {
+      className: "modal__header__closeBtn",
+      onClick: () => closeModal()
+    }, /*#__PURE__*/React.createElement("img", {
+      className: "modal__header__closeBtn__img",
+      src: closeBtn,
+      alt: "Fermer"
+    }))), /*#__PURE__*/React.createElement("div", {
+      className: "modal__main-content"
+    }, /*#__PURE__*/React.createElement("img", {
+      className: "modal__main-content__icon",
+      src: icon,
+      alt: "Icone"
+    }), /*#__PURE__*/React.createElement("h1", {
+      className: "modal__main-content__title"
+    }, title)), /*#__PURE__*/React.createElement("div", {
+      className: "modal__main-content"
+    }, /*#__PURE__*/React.createElement("p", {
+      className: "modal__main-content__message"
+    }, message)), /*#__PURE__*/React.createElement("button", {
+      className: 'modal__closeBtn modal__closeBtn--' + type,
+      onClick: () => closeModal()
+    }, "Fermer")));
+  }
+}
+
+//Allows to set the default icons to be displayed for the predefined types.
+function getDefaultIcons(type) {
+  let defaultIcon;
+  switch (type) {
+    case 'warning':
+      defaultIcon = warningLogo;
+      break;
+    case 'informative':
+      defaultIcon = informativeLogo;
+      break;
+    case 'success':
+      defaultIcon = successLogo;
+      break;
+    case 'error':
+      defaultIcon = errorLogo;
+      break;
+    default:
+      console.log("An error has occurred. The default icons are not available.");
+      break;
+  }
+  return defaultIcon;
+}
+function enableScroll() {
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.remove("no-scroll-modal");
+}
+function disableScroll() {
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.add("no-scroll-modal");
+}
+
+export { Modal };
